ace6f5ddd7edecd52a94bb6421fa553175502c02
bernard0331 <47488073+bernard0331@users.noreply.github.com>
2024-04-16 13:40:00 -0400

    added autoclicker stuff, do not uncomment the .click

--- screen_capture.py
+++ screen_capture.py
@@ -1,16 +1,24 @@
 import numpy as np
 import cv2
+import pyautogui
 from mss import mss
 from PIL import Image
 
-bounding_box = {'top': 100, 'left': 0, 'width': 400, 'height': 300}
+def mouse_click(event, x, y, flags, param): 
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print("Left button pressed")
+    else:
+        print("nothing is happening")
 
-sct = mss()
+screenWidth, screenHeight = pyautogui.size()
+bounding_box = {'top': screenHeight/2 + 50, 'left': 0, 'width': screenWidth/2, 'height': screenHeight/2 - 50}
+
+cv2.setMouseCallback('screen', mouse_click)
 
 while True:
-    sct_img = sct.grab(bounding_box)
+    sct_img = mss().grab(bounding_box)
     cv2.imshow('screen', np.array(sct_img))
-
+    #pyautogui.click()
     if (cv2.waitKey(1) & 0xFF) == ord('q'):
         cv2.destroyAllWindows()
         break
