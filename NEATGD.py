b1256b996e24a73821b3be54e48f88dad42af292
Justin Kerns <justinkerns001@gmail.com>
2024-04-23 21:28:32 -0400

    Completed NEAT implementation

--- NEATGD.py
+++ NEATGD.py
@@ -1,8 +1,8 @@
 from FrameHelper import FrameProcessor
-from NEATHelper import NEATController
+from NEATController import NEATController
 import os
 
-#from PIL import Image
+from PIL import Image
 
 #Constants for the window titles of Geometry Dash and The Impossible Game for easy switching
 GD = "Geometry Dash"
@@ -15,24 +15,29 @@ frame_processor = FrameProcessor(GAME,60,100,240,160)
 
 # The amount of fps we can achieve with the current     
 # hardware and preprocessing.
-print("FPS: ", frame_processor.calculate_fps())
+#print("FPS: ", frame_processor.calculate_fps())
 
 # Displays the frames of geometry dash for 30 seconds.
 # Feel free to change the duration.
 #print(frame_processor.display_frames(30))
 
 
-
-#img = Image.fromarray(frame_processor.get_raw_frame(35,180,330,500))
+#img = frame_processor.get_raw_frame(35,180,330,100)
+#img = Image.fromarray(img)
 #img.show()
 
 #Runs NEAT
 local_dir = os.path.dirname(__file__)
 config_path = os.path.join(local_dir, 'config-feedforward')
 
+saveload_path = os.path.join(local_dir, 'cache.pkl')
+
 print("Starting NEAT")
 nc = NEATController(config_path, frame_processor)
-nc.evolve(1)
-print("Winner decided, showing final model")
-nc.eval()
+nc.evolve(10)
+print("Winner decided, saving winner")
+nc.save_model(saveload_path)
+print("Saved, playing final model")
+progress = nc.eval()
+print(progress)
 nc.show_model()
